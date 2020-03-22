de7effe12d23e28dff92f62967d52a58c2c99823
Andreas Limi <andreaslimidev@gmail.com>
2020-03-22 13:10:12 +0100

    init

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+export default () => (
+    <Layout>
+    <h1>
+        Log in to app brother
+    </h1>
+    <Link to="/dashboard">Go to dashboard </Link>
+    </Layout>
+)
